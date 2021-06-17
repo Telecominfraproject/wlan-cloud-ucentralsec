e8eed6a66857b8a32a91fc6121389520e674ad35
stephb9959 <stephane.bourque@gmail.com>
2021-06-17 14:21:09 -0700

    Initial

--- src/Daemon.cpp
+++ src/Daemon.cpp
@@ -20,6 +20,7 @@
 #include "Poco/Util/HelpFormatter.h"
 #include "Poco/Environment.h"
 #include "Poco/Path.h"
+#include "Poco/File.h"
 #include "Poco/Net/SSLManager.h"
 
 #include "Utils.h"
@@ -83,24 +84,25 @@ namespace uCentral {
         loadConfiguration(ConfigFile.toString());
 
         if(LogDir_.empty()) {
-            std::string OriginalLogFileValue = config().getString(LogFilePathKey);
-            std::string RealLogFileValue = Poco::Path::expand(OriginalLogFileValue);
-            config().setString(LogFilePathKey, RealLogFileValue);
+            std::string OriginalLogFileValue = ConfigPath(LogFilePathKey);
+            config().setString(LogFilePathKey, OriginalLogFileValue);
         } else {
             config().setString(LogFilePathKey, LogDir_);
         }
-
-        Poco::Path	DataDir(config().getString("system.directory.data"));
-        try {
-            DataDir.makeDirectory();
-            DataDir_ = DataDir.toString();
-        } catch(...) {
+        Poco::File	DataDir(ConfigPath("ucentral.system.data"));
+        DataDir_ = DataDir.path();
+        if(!DataDir.exists()) {
+            try {
+                DataDir.createDirectory();
+            } catch (const Poco::Exception &E) {
+                logger().log(E);
+            }
         }
-        std::string KeyFile = Poco::Path::expand(config().getString("ucentral.service.key"));
+        std::string KeyFile = ConfigPath("ucentral.service.key");
         AppKey_ = Poco::SharedPtr<Poco::Crypto::RSAKey>(new Poco::Crypto::RSAKey("", KeyFile, ""));
+        ID_ = ConfigGetInt("ucentral.system.id",1);
         if(!DebugMode_)
-            DebugMode_ = config().getBool("ucentral.system.debug",false);
-        ID_ = config().getInt64("ucentral.system.id",1);
+            DebugMode_ = ConfigGetBool("ucentral.system.debug",false);
 
         InitializeSubSystemServers();
         logger().information("Starting...");
@@ -276,11 +278,19 @@ namespace uCentral {
     }
 
     std::string Daemon::ConfigGetString(const std::string &Key,const std::string & Default) {
+        return config().getString(Key, Default);
+    }
+
+    std::string Daemon::ConfigGetString(const std::string &Key) {
+        return config().getString(Key);
+    }
+
+    std::string Daemon::ConfigPath(const std::string &Key,const std::string & Default) {
         std::string R = config().getString(Key, Default);
         return Poco::Path::expand(R);
     }
 
-    std::string Daemon::ConfigGetString(const std::string &Key) {
+    std::string Daemon::ConfigPath(const std::string &Key) {
         std::string R = config().getString(Key);
         return Poco::Path::expand(R);
     }
