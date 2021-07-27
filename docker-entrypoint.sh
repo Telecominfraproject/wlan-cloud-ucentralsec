9743508520e1d54a08fee22878d748e72056704b
oblom0v <johann.hoffmann@mailbox.org>
2021-07-27 11:22:12 +0200

    Change to colon separator

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,7 +2,7 @@
 set -e
 
 if [ "$1" = '/ucentral/ucentralsec' -a "$(id -u)" = '0' ]; then
-    chown -R "$UCENTRALSEC_USER". "$UCENTRALSEC_ROOT" "$UCENTRALSEC_CONFIG"
+    chown -R "$UCENTRALSEC_USER": "$UCENTRALSEC_ROOT" "$UCENTRALSEC_CONFIG"
     exec su-exec "$UCENTRALSEC_USER" "$@"
 fi
 
